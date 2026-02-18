71de7dfa8c5eae21badcd5724ce6ffb651a0c72c
thi4go028 <thi4go028@gmail.com>
2026-02-18 11:47:07 -0300

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from 'next'
 import { Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
+import Script from 'next/script'
 import './globals.css'
 
 const geistMono = Geist_Mono({
@@ -42,6 +43,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={geistMono.variable}>
+      <head>
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1455796019462309');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+      </head>
       <body className="font-mono antialiased">
         {children}
         <Analytics />
